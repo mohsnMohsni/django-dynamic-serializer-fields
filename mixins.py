3678015f4183fb5a4e229ffdbad5888748edec11
Mohsen Mohseni <70364834+mohsnMohsni@users.noreply.github.com>
2022-02-22 16:53:49 +0330

    Update mixins.py

--- mixins.py
+++ mixins.py
@@ -1,14 +1,3 @@
-class RestDynamicViewSerializerFields:
-    serializer_fields = None
-
-    def get_serializer(self, *args, **kwargs):
-        return super().get_serializer(fields=self.serializer_fields, *args, **kwargs)
-
-    def check_serializer_fields(self) -> None:
-        if self.serializer_fields and not isinstance(self.serializer_fields, (tuple, list)):
-            raise TypeError("'serializer_fields' must be tuple or list instance.")
-
-
 class RestDynamicSerializerFields:
     def __init__(self, *args, **kwargs):
         self.custom_fields = kwargs.pop('fields', None)
@@ -21,3 +10,23 @@ class RestDynamicSerializerFields:
             modifiy_fields = existing.intersection(allowed)
             return tuple(modifiy_fields)
         return fields_name
+
+
+class RestDynamicViewSerializerFields:
+    serializer_fields = None
+
+    def __init__(self, *args, **kwargs) -> None:
+        self.check_serializer_class_is_subclass_of_dynamic_fields()
+        self.check_serializer_fields()
+        super().__init__(*args, **kwargs)
+
+    def get_serializer(self, *args, **kwargs):
+        return super().get_serializer(fields=self.serializer_fields, *args, **kwargs)
+
+    def check_serializer_fields(self) -> None:
+        if self.serializer_fields and not isinstance(self.serializer_fields, (tuple, list)):
+            raise TypeError("'serializer_fields' must be tuple or list instance.")
+
+    def check_serializer_class_is_subclass_of_dynamic_fields(self) -> None:
+        if not issubclass(self.serializer_class, RestDynamicSerializerFields):
+            raise NotImplementedError("serializer class should inheritance from 'RestDynamicSerializerFields'")
