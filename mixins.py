7b5859e4535b5ae1eb5c5f7c0ba6c6beb978d789
Mohsen Mohseni <70364834+mohsnMohsni@users.noreply.github.com>
2022-02-22 16:24:03 +0330

    Update mixins.py

--- mixins.py
+++ mixins.py
@@ -1,22 +1,23 @@
-class DynamicSerializerFields:
+class RestDynamicViewSerializerFields:
     serializer_fields = None
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.check_serializer_fields()
-        self.serializer_class.__init__ = self.overridden_init
-
-    @staticmethod
-    def overridden_init(self, *args, **kwargs) -> None:
-        super(self.__class__, self).__init__(*args, **kwargs)
-        view = self.context.get('view')
-        fields = getattr(view, 'serializer_fields', None)
-
-        if fields is not None:
-            allowed, existing = set(fields), set(self.fields)
-            for field_name in existing - allowed:
-                self.fields.pop(field_name)
+    def get_serializer(self, *args, **kwargs):
+        return super().get_serializer(fields=self.serializer_fields, *args, **kwargs)
 
     def check_serializer_fields(self) -> None:
         if self.serializer_fields and not isinstance(self.serializer_fields, (tuple, list)):
             raise TypeError("'serializer_fields' must be tuple or list instance.")
+
+
+class RestDynamicSerializerFields:
+    def __init__(self, *args, **kwargs):
+        self.custom_fields = kwargs.pop('fields', None)
+        super().__init__(*args, **kwargs)
+
+    def get_field_names(self, *args, **kwargs):
+        fields_name = super().get_field_names(*args, **kwargs)
+        if self.custom_fields:
+            allowed, existing = set(self.custom_fields), set(fields_name)
+            modifiy_fields = existing.intersection(allowed)
+            return tuple(modifiy_fields)
+        return fields_name
